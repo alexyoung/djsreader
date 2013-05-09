c9f9d06258f4973018a1cc48c226642bbb32938f
Alex Young <alex@helicoid.net>
2013-05-09 15:00:14 +0100

    Part 4: Managing feeds

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,41 @@
 'use strict';
 
 angular.module('djsreaderApp')
-  .controller('MainCtrl', function($scope, $http) {
-    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+  .controller('MainCtrl', function($scope, $http, $timeout) {
+    $scope.refreshInterval = 60;
+    $scope.feeds = [{
+      url: 'http://dailyjs.com/atom.xml'
+    }];
 
-    $http.jsonp(url).
-      success(function(data, status, headers, config) {
-        $scope.feed = {
-          title: 'DailyJS',
-          items: data.query.results.entry
-        };
-      }).
-      error(function(data, status, headers, config) {
-        console.error('Error fetching feed:', data);
-      });
+    $scope.fetchFeed = function(feed) {
+      feed.items = [];
+
+      var apiUrl = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'";
+      apiUrl += encodeURIComponent(feed.url);
+      apiUrl += "'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+      $http.jsonp(apiUrl).
+        success(function(data, status, headers, config) {
+          if (data.query.results) {
+            feed.items = data.query.results.entry;
+          }
+        }).
+        error(function(data, status, headers, config) {
+          console.error('Error fetching feed:', data);
+        });
+
+      $timeout(function() { $scope.fetchFeed(feed); }, $scope.refreshInterval * 1000);
+    };
+
+    $scope.addFeed = function(feed) {
+      $scope.feeds.push(feed);
+      $scope.fetchFeed(feed);
+      $scope.newFeed = {};
+    };
+
+    $scope.deleteFeed = function(feed) {
+      $scope.feeds.splice($scope.feeds.indexOf(feed), 1);
+    };
+
+    $scope.fetchFeed($scope.feeds[0]);
   });
